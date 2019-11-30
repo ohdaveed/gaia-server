0184250f8b40f567dc93ccc47263223ac4af9daf
David Arrizon <arrizon.david@gmail.com>
2019-11-30 16:20:22 -0600

    created validation dir and register.js

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express();
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 
+//BodyParser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//DB Config
 require("./db/db.js");
 
 app.get("/", (req, res) => res.send("Hello world!"));
