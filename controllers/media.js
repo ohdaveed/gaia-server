aab554766ab09e435f618c14ee98a945f381e6e9
David Arrizon <arrizon.david@gmail.com>
2019-12-02 11:59:17 -0600

    can upload files to cloudinary media library

--- controllers/media.js
+++ controllers/media.js
@@ -19,7 +19,33 @@ router.post("/upload", (req, res, next) => {
 		if (err) {
 			return res.send(err);
 		}
-		res.json(req.file);
+		// res.json(req.file);
+		console.log("file uploaded to server");
+		console.log(req.file);
+
+		const cloudinary = require("cloudinary").v2;
+		cloudinary.config({
+			cloud_name: process.env.CLOUD_NAME,
+			api_key: process.env.CLOUDINARY_API_KEY,
+			api_secret: process.env.CLOUDINARY_SECRET
+		});
+
+		const path = req.file.path;
+		const uniqueFilename = new Date().toISOString();
+
+		cloudinary.uploader.upload(
+			path,
+			{ public_id: `gaia/${uniqueFilename}`, tags: `gaia` }, // directory and tags are optional
+			function(err, image) {
+				if (err) return res.send(err);
+				console.log("file uploaded to Cloudinary");
+				// remove file from server
+				const fs = require("fs");
+				fs.unlinkSync(path);
+				// return image details
+				res.json(image);
+			}
+		);
 	});
 });
 
