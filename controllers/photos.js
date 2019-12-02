764a6fe05ec5023b0c4bf64345b92f2e6227b25f
David Arrizon <arrizon.david@gmail.com>
2019-12-02 15:36:06 -0600

    created and tested delete photo route

--- controllers/photos.js
+++ controllers/photos.js
@@ -89,4 +89,13 @@ router.post("/upload", (req, res, next) => {
 	});
 });
 
+// @route GET api/photos/:id
+// @description Delete photo by id
+// @access Public
+router.delete("/:id", (req, res) => {
+	Photo.findByIdAndRemove(req.params.id, req.body)
+		.then((photo) => res.json({ mgs: "Photo entry deleted successfully" }))
+		.catch((err) => res.status(404).json({ error: "No such photo" }));
+});
+
 module.exports = router;
