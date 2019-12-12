90ad760cc1656667ebc52eb78112cd19617ce02e
David Arrizon <arrizon.david@gmail.com>
2019-12-12 00:36:16 -0600

    set up plant api routes with test route

---
+++ routes/api/plants.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const { encode, decode } = require("url-encode-decode");
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
+const User = require("../../models/User");
+const keys = require("../../config/keys");
+const Photo = require("../../models/Photo");
+const Plant = require("../../models/Plants");
+
+const passport = require("passport");
+
+router.get("/", passport.authenticate("jwt", { session: false }), function(
+    req,
+    res
+) {
+    res.send("photo route testing!");
+});
+
+module.exports = router;
