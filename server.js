2447d34b76617d70e3fccdfbfdc1c0ce1bb04152
David Arrizon <arrizon.david@gmail.com>
2020-03-27 02:09:38 -0500

    8000 to 300

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ require("./db/db.js");
 // Connect to MongoDB
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
