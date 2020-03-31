de27cf60dd2de9e6b3d6bba49bd4a84069b1c7f5
David Arrizon <arrizon.david@gmail.com>
2020-03-30 19:58:54 -0500

    mongoose

--- server.js
+++ server.js
@@ -1,34 +1,25 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const passport = require("passport");
-const users = require("./routes/api/users");
-const photos = require("./routes/api/photos");
-const plants = require("./routes/api/plants");
 
-//DB Config
-require("./db/db.js");
+const helmet = require('helmet');
+const mongoose = require('mongoooe');
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const users = require('./routes/api/users');
+const photos = require('./routes/api/photos');
+const plants = require('./routes/api/plants');
+
+// DB Config
+require('./db/db.js');
 
 // Connect to MongoDB
-let whitelist = ['http://localhost:3000', 'https://gaiadb.herokuapp.com']
+app.use(helmet());
 
-app.use(cors({
-  origin: function(origin, callback){
-
-    if(!origin) return callback(null, true);
-
-    if(whitelist.indexOf(origin) === -1){
-      let message = 'shant pass'
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
-
-//BodyParser
+// BodyParser
 // app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -36,15 +27,19 @@ app.use(bodyParser.json());
 // Passport middleware
 app.use(passport.initialize());
 // Passport config
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
 // Routes
-app.use("/api/users", users);
-app.use("/api/photos", photos);
-app.use("/api/plants", plants);
-app.get("/", (req, res) => {
-  res.send("Gaia backend");
+app.use('/api/users', users);
+app.use('/api/photos', photos);
+app.use('/api/plants', plants);
+app.get('/', (req, res) => {
+  res.send('Gaia backend');
 });
 
-app.listen(process.env.PORT || 8000);
+const port = process.env.PORT || 8000;
 
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Express server listening on port ${port}.`);
+});
