adf7a8d36c78c26e477113d17ea2e6a951eefd82
David Arrizon <arrizon.david@gmail.com>
2020-04-03 22:12:00 -0500

    added prettier

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ const plants = require('./routes/api/plants');
 // Connect to MongoDB
 // app.use(helmet());
 
-app.use(cors());
-
+app.use(cors({origin: ['http://localhost:3000']}));
 // BodyParser
 // app.use(methodOverride("_method"));
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
 
 // DB Config
 require('./db/db.js');
@@ -35,13 +35,15 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/photos', photos);
 app.use('/api/plants', plants);
-// app.get('/', (req, res) => {
-//   res.send('Gaia backend');
-// });
+app.get('/', (req, res) => {
+	const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }))
+
+});
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-	// eslint-disable-next-line no-console
 	console.log(`Express server listening on port ${port}.`);
 });
